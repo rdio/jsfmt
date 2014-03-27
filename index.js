b6e73d2910745dc53bf90387191ba7250064e935
Jim Fleming <jimmy.fleming@gmail.com>
2014-03-27 16:41:17 -0700

    Using new formatter: esformatter since it preserves whitespace and has more formatting options

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 exports.rewriteJavascript = require('./rewrite.js').rewriteJavascript;
 exports.findJavascript = require('./rewrite.js').findJavascript;
-exports.formatJavascript = require('./format.js').formatJavascript;
