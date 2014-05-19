e8cae50492f06c846e035d3dc74d71281b92de9e
Gilad Peleg <giladp007@gmail.com>
2014-05-19 20:48:30 +0300

    close #26 - organize project

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-exports.rewrite = require('./rewrite.js').rewrite;
-exports.search = require('./rewrite.js').search;
+var rewritePath = './lib/rewrite.js';
+
+exports.rewrite = require(rewritePath).rewrite;
+exports.search = require(rewritePath).search;
