4173cabaacaec5ae6607a43d8a399345def21e75
Jim Fleming <jimmy.fleming@gmail.com>
2014-05-21 10:00:59 -0700

    Adding support for wildcard lists

--- tests/test.js
+++ tests/test.js
@@ -16,14 +16,21 @@ describe('jsfmt', function() {
     jsfmt.rewrite('_.reduce(a,b,c)', '_.reduce(a, b, c) -> a.reduce(b, c)')
       .toString().should.eql('a.reduce(b, c)');
   });
+
   it('should test basic searching', function() {
-    var results = jsfmt.search('var param1 = 1, done= function(){}; _.each(param1, done);', '_.each(a, b);');
+    var results = jsfmt.search('var param1 = 1, done = function() {}; _.each(param1, done);', '_.each(a, b);');
     results[0].wildcards.a.name.should.eql('param1');
     results[0].wildcards.b.name.should.eql('done');
   });
+
   it('should test basic formatting', function() {
     var js = 'var func = function(test){console.log( test );};';
     var result = jsfmt.format(js, {});
     result.should.eql('var func = function(test) {\n  console.log(test);\n};');
+  });
+
+  it('should transform function args during rewrite', function() {
+    jsfmt.rewrite('jade_mixins["my_key"](argA, argB, argC)', 'jade_mixins[a]($b) -> templates[a]($b)')
+      .toString().should.eql("templates['my_key'](argA, argB, argC)");
   });
 });
