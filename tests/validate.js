dc9d4ef1d4f1938ae4228a2d837ece433120365c
brettlangdon <brett@blangdon.com>
2015-11-02 09:46:59 -0500

    move validate to use fixtures

--- tests/validate.js
+++ tests/validate.js
@@ -3,13 +3,14 @@
 'use strict';
 var should = require('should');
 var fs = require('fs');
+var utils = require('./utils');
 
 var libPath = process.env.JSFMT_COV ? 'lib-cov' : 'lib';
 var jsfmt = require('../' + libPath + '/index');
 
 describe('jsfmt.validate', function() {
   it('should test basic validation', function() {
-    var js = 'return 42;\nvar func = function(test){console.log( test );};';
+    var js = utils.loadFixture('illegal-return.js');
     var errors = jsfmt.validate(js);
     errors.should.have.length(1);
     errors[0].index.should.eql(6);
@@ -19,7 +20,7 @@ describe('jsfmt.validate', function() {
   });
 
   it('should test shebangs', function() {
-    var js = '#!/usr/bin/env node\nvar func = function(test){console.log( test );};';
+    var js = utils.loadFixture('shebang.js');
     var errors = jsfmt.validate(js);
     errors.should.have.length(0);
   });
@@ -27,13 +28,13 @@ describe('jsfmt.validate', function() {
 
 describe('jsfmt.validateJSON', function() {
   it('should test validation json object', function() {
-    var js = '{"hello": "world"}';
+    var js = utils.loadFixture('basic.json');
     var errors = jsfmt.validateJSON(js);
     errors.should.have.length(0);
   });
 
   it('should test validation json array', function() {
-    var js = '["hello", "world"]';
+    var js = utils.loadFixture('array.json');
     var errors = jsfmt.validateJSON(js);
     errors.should.have.length(0);
   });
